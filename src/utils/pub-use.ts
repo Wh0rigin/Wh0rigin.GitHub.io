9c62e06958f592fc1d951348b80e15acd7431610
Lightsake-Sensei <805935229@qq.com>
2024-01-02 23:47:27 +0800

    图片内部引用

--- src/utils/pub-use.ts
+++ src/utils/pub-use.ts
@@ -2,9 +2,6 @@ const getAssets = function(path:string): string {
     return new URL(path, import.meta.url).href;
 };
 
-const toPublic = function(path:string): string {
-    return new URL(path, import.meta.url).href;
-};
 
 
 export default getAssets;
